3fe60421583cf2f038a92f323686ba275cd7ab95
Allencl <Allen__cc@163.com>
2021-09-27 09:12:51 +0800

    11

---
+++ src/layout/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class HomeHTML extends React.Component {
+
+  state = {
+
+  };
+
+  render() {
+
+    return (
+        <div>home</div>
+    );
+  }
+}
+
+export default HomeHTML; 
